e35549a287d9076e7a7e011680e5838f3e5395ab
Hang52z <hangzhao221@gmail.com>
2025-07-20 16:06:27 +1000

    update api address

--- frontend/src/api/api.ts
+++ frontend/src/api/api.ts
@@ -3,7 +3,7 @@ import type { Announcement } from "./types";
 
 
 export async function fetchAnnouncements(): Promise<Announcement[]> {
-  const response = await fetch("http://localhost:8000/announcements");
+  const response = await fetch("https://asx-analysis.vercel.app/announcements.csv")
   if (!response.ok) {
     throw new Error("Failed to fetch announcements");
   }
